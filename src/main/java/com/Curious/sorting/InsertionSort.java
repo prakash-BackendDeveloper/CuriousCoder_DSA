304104980c4c60ea9609ccb10ff32c44d16bd1a0
prakash-4u <prakash96m@gmail.com>
2025-01-09 13:37:28 +0530

    Insertion sort

---
+++ src/main/java/com/Curious/sorting/InsertionSort.java
@@ -0,0 +1,19 @@
+package com.Curious.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int a[]={4,1,5,3,90,65,34,8,9};
+        for(int i=0;i<a.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(a[j]<a[j-1]){
+                    int temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
