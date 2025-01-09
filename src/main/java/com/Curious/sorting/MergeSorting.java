d3650aca273fcf0b8c7f82cb9ef20d1f64e0f327
prakash-4u <prakash96m@gmail.com>
2025-01-09 14:16:25 +0530

    merge sort

---
+++ src/main/java/com/Curious/sorting/MergeSorting.java
@@ -0,0 +1,50 @@
+package com.Curious.sorting;
+
+public class MergeSorting {
+    static void merge(int a[],int start,int mid,int end){
+        int temp[]=new int[end-start+1];
+        int i=start,j=mid+1,k=0;
+        while(i<=mid && j<=end){
+            if(a[i]<a[j]){
+                temp[k]=a[i];
+                k++;
+                i++;
+            }
+            else{
+                temp[k]=a[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<=mid){
+            temp[k]=a[i];
+            k++;
+            i++;
+        }
+        while(j<=end){
+            temp[k]=a[j];
+            k++;
+            j++;
+        }
+        int x=0;
+        for(i=start;i<=end;i++){
+            a[i]=temp[x];
+            x++;
+        }
+
+    }
+    static void mergeSort(int[] a, int start, int last) {
+        if(start>=last) return;
+        int mid = (start+last)/2;
+        mergeSort(a, start, mid);
+        mergeSort(a, mid+1, last);
+        merge(a,start,mid,last);
+    }
+    public static void main(String[] args) {
+        int a[]={4,1,5,3,90,65,34,8,9};
+        mergeSort(a,0,a.length-1);
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
