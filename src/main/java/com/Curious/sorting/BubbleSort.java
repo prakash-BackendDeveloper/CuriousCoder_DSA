0f204b279c6db18a112ac3c20133a4f675b0d75e
prakash-4u <prakash96m@gmail.com>
2025-01-09 13:20:52 +0530

    BubbleSort

---
+++ src/main/java/com/Curious/sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.Curious.sorting;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int a[]={1,2,9,6,4,8};
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
